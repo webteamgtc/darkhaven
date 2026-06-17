01b0f20b0db8ff9eb5949a827547332e9ad37606
webteamm <webteam@gtcfx.com>
2026-06-17 16:14:23 +0400

    Remove .next from tracking, update .gitignore

--- .next/dev/static/chunks/src_app_page_tsx_19t1i3d._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_05aezzj._.js","static/chunks/node_modules_1xt7zgx._.js"],
-    source: "dynamic"
-});
